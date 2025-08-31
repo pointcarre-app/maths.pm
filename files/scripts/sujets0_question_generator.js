0951fb8ebd36cb737ae7fb88f7bd3e2074c3b96d
Selimmmm <selim.mellouk.halgand@proton.me>
2025-09-01 00:20:52 +0200

    Chrome done

--- files/scripts/sujets0_question_generator.js
+++ files/scripts/sujets0_question_generator.js
@@ -470,10 +470,12 @@ function addPrintButton(container) {
                           d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z">
                     </path>
                 </svg>
-                Imprimer les questions (marges nulles)
+                Imprimer les questions
             </button>
-            <div class="text-xs text-gray-600 print-hide">
-                💡 Dans la boîte de dialogue d'impression, sélectionnez "Marges: Aucune"
+            <div class="text-xs text-gray-600 print-hide text-right">
+                 Optimisé pour Chrome ✅<br>
+                 Activer les "Graphiques d'arrière-plan" 💡<br>
+                 Possibilité d'ajuster les marges 🛠️
             </div>
         </div>
     `;
